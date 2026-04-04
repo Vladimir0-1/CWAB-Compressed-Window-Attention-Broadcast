e40e590f8aabc54e134acc7047d92eee5c014502
Vladimir0-1 <vova.ignatev.12@bk.ru>
2026-04-04 17:12:54 +0300

    Create __init__.py

---
+++ hsa/__init__.py
@@ -0,0 +1,10 @@
+"""
+Hybrid State-Space Attention (HSA) - Linear-complexity attention mechanism
+Author: Vladimir0-1
+License: MIT
+"""
+
+from .attention import HybridStateSpaceAttention
+
+__all__ = ["HybridStateSpaceAttention"]
+__version__ = "0.1.0"
